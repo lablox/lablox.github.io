1f23e72efb0d7d02aec8700f1eeaf46124deeee7
Mehran Baghi <1mehranbaghi@gmail.com>
2021-07-13 09:52:37 +0430

    [skip ci]

--- build.sh
+++ build.sh
@@ -12,13 +12,13 @@ rm 404_body.html 404_m4ed.html 404.css 404.md
 }
 
 md2html () {
-cp $maindir/main.css .
+cp $maindir/$3 .
 name=$(echo $1 | cut -d'.' -f1)
 pandoc -f markdown-auto_identifiers -t html5 --lua-filter=$maindir/builder/auto_identifiers_underscore.lua $1 -o body.html
 cat $1 | head -n1 | sed -E "s|^#\s||g;s|\s$||g" > title
-cat $maindir/builder/header.html body.html $maindir/builder/footer.html | m4 > $name-premini.html
+cat $maindir/builder/$2 body.html $maindir/builder/footer.html | m4 > $name-premini.html
 html-minifier -c $maindir/builder/html-minifier.conf $name-premini.html > $name.html
-rm body.html $name-premini.html title main.css $1
+rm body.html $name-premini.html title $1 $3
 }
 
 recursivemd2html () {
@@ -30,25 +30,31 @@ do
         recursivemd2html
         cd ..
     else
-        if [[ $obj = "404.md" ]]
-        then
-           404md2html
-        else
-           md2html $obj
-        fi
+        md2html $obj page_header.html main.css
     fi
 done
 }
 
+lintmini () {
+stylelint ./builder/$1 --config ./builder/stylelint.json || exit 1
+postcss --use cssnano -o ./$1 ./builder/$1
+}
+
 maindir=$(pwd)
-stylelint ./builder/main.css --config ./builder/stylelint.json || exit 1
-postcss --use cssnano -o ./main.css ./builder/main.css
-stylelint ./builder/404.css --config ./builder/stylelint.json || exit 1
-postcss --use cssnano -o ./404.css ./builder/404.css
+lintmini main.css
+lintmini home.css
+lintmini 404.css
 rm -rf docs
 cp -r markdown docs
-cd docs
+mkdir h404
+mv docs/index.md docs/404.md h404
+mv home.css 404.css h404
+cd h404
+md2html index.md home_header.html home.css
+404md2html
+cd ../docs
 recursivemd2html
 cd $maindir
+mv h404/index.html h404/404.html docs
 cp ./builder/imorty.png docs
-rm main.css 404.css
+rm main.css h404
